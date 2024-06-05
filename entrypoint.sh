e4675dff903522b1bf389da38cd6631a44a8d575
Sygmei <3835355+Sygmei@users.noreply.github.com>
2024-06-06 00:47:25 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-PYTHONPATH=.:$PYTHONPATH python sho7/database/initialisation.py && python sho7/app.py
+PYTHONPATH=.:$PYTHONPATH python sho7/database/initialisation.py
+PYTHONPATH=.:$PYTHONPATH python sho7/app.py
