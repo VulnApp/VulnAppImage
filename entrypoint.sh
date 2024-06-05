3aa5112831f7f64d902d86cb50690c09f370991c
Sygmei <3835355+Sygmei@users.noreply.github.com>
2024-06-05 10:28:13 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-opentelemetry-instrument gunicorn --bind 0.0.0.0 --reload --env SCRIPT_NAME=$ROOT_PATH sho7.app:app
+gunicorn --bind 0.0.0.0 --reload --env SCRIPT_NAME=$ROOT_PATH sho7.app:app
